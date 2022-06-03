9fd84d72913246678bb74c3bcffef62de3668689
Luis Mariano Bibo <lmbibbo@gmail.com>
2022-06-03 14:05:12 -0300

    Primer Commit

---
+++ src/test/java/com/example/springdockerexample/SpringDockerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdockerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
