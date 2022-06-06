6afed9115d12d16c4047c6d3243bbcf29f488c72
Luis Mariano Bibbo <lmbibbo@gmail.com>
2022-06-06 17:27:01 -0300

    Con Controller

---
+++ src/main/java/com/example/springdockerexample/endpoints/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.springdockerexample.endpoints;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping(value = "/")
+	public ResponseEntity<String> greeting() {
+		return ResponseEntity.ok("Hola mundo! Luisma");
+	}
+}
