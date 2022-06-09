8506a97444319483d750f7473691044c13ae32f1
Luis Mariano Bibo <lmbibbo@gmail.com>
2022-06-09 11:02:18 -0300

    Initial Structure

--- src/main/java/com/example/springdockerexample/controllers/HelloController.java
+++ src/main/java/com/example/springdockerexample/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.springdockerexample.endpoints;
+package com.example.springdockerexample.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
